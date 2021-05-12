86636e4f2c176d6a325ab4b2388dbbf581f74a9b
Boyd Harghaz <bharghaz@pl-b1.msk.21-school.ru>
2021-05-12 16:26:36 +0300

    save

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # school21_GNL
+
+
+   * char *ft_saverstr(const char *buffer, char *saver)
+check if saver != NULL
+NULL means first iter
+first iter just copies else concats
+returns result (itself or concatinated strs)
+with \0 termination.
--- get_next_line.c
+++ get_next_line.c
@@ -1,5 +1,24 @@
+# define BUFFER_SIZE 8
+
 int	get_next_line(int fd, char **line)
 {
-	char	*buffer;
-	
+	static char	*saver;
+	char		*buffer;
+	int			bytes;
+
+	line = (char **)malloc(sizeof(char*));
+	while (bytes)
+	{
+		bytes = read(fd, buffer, BUFFER_SIZE);
+		if (bytes == -1)
+			return (bytes);
+		saver = ft_savestr(buffer, saver);	
+		if (ft_strchr(bytes, "\n"))			
+		{
+			*line = (char *)malloc(ft_strlen(saver) + 1);
+			*line = saver;					
+		}								
+											
+											
+	}
 }
