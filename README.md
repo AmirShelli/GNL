68480b078b0e94aaf6a4974dd0959aa145c27f7e
Boyd Harghaz <bharghaz@pl-b1.msk.21-school.ru>
2021-05-12 16:41:07 +0300

    save

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # school21_GNL
 
 
-   * char *ft_saverstr(const char *buffer, char *saver)
+   * char *ft_saverstr(char *saver, const char *buffer)
 check if saver != NULL
-NULL means first iter
-first iter just copies else concats
+NULL means first iter for saver
+first iter malloc for saver then copy,
+else concatinates until \n in a aux str, 
+frees saver and fills it ready to be returned, 
 returns result (itself or concatinated strs)
 with \0 termination.
--- get_next_line.c
+++ get_next_line.c
@@ -6,19 +6,19 @@ int	get_next_line(int fd, char **line)
 	char		*buffer;
 	int			bytes;
 
-	line = (char **)malloc(sizeof(char*));
+	line = (char **)malloc(sizeof(char *));
 	while (bytes)
 	{
 		bytes = read(fd, buffer, BUFFER_SIZE);
 		if (bytes == -1)
 			return (bytes);
-		saver = ft_savestr(buffer, saver);	
-		if (ft_strchr(bytes, "\n"))			
+		saver = ft_savestr(saver, buffer);
+		if (ft_strchr(buffer, "\n"))
 		{
 			*line = (char *)malloc(ft_strlen(saver) + 1);
-			*line = saver;					
+			*line = saver;
+			saver = ft_savestr(saver, buffer + 1); // + 1 if buffer[0] == \n 
+			return (1);
 		}								
-											
-											
 	}
 }
